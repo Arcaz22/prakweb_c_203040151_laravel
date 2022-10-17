04dd274b3a8adfd16dc37643ec512cf21b10ee72
Arcaz22 <chandraarcychan@gmail.com>
2022-10-17 21:41:37 +0700

    vidio 3 -  Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CAA Blog | About</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  
-  <h1>Halaman About</h1>
-  <h3><?php echo $name; ?></h3>
-  <p><?php echo $email; ?></p>
-  <img src="img/<?php echo $image; ?>" alt="<?php echo $name; ?>" width="200">
+@extends('layouts.main')
 
-</body>
-</html>
+@section('container')
+  <h1>Halaman About</h1>
+  <h3>{{ $name }}</h3>
+  <p>{{ $email }}</p>
+  <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+@endsection
