871cb6a78deaf991ac50e9bae7692912cc48ae3a
Arcaz22 <chandraarcychan@gmail.com>
2022-10-24 10:59:54 +0700

    vidio ke 9 - Database Seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,12 +3,14 @@
 @section('container')
 <h1 class="mb-4">Halaman Blog Posts</h1>
   @foreach ($posts as $post) 
-  <article>
-    <h2>
-      <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+  <article class="mb-5 border-bottom pb-3">
+    <h2 class="text-decoration-none">
+      <a class="text-decoration-none" href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
     </h2>
+    <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> <a class="text-decoration-none" href="/categories/{{ $post->category->slug  }}">{{ $post->category->name }}</a></p>
     {{-- <h5>{{ $post->author }}</h5> --}}
     {{ $post->excerpt }}
+    <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read More...</a>
   </article>
   @endforeach
 
