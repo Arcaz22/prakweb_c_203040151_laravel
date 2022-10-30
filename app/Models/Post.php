dad7936763220aa054e7daf67dd9e39cd793541b
Arcaz22 <chandraarcychan@gmail.com>
2022-10-31 00:08:48 +0700

    vidio 18 - Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use HasFactory, Sluggable;
+
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, function($query, $search) {
@@ -44,5 +47,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
